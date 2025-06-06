9b5d2d504e026cd71281fbc1adec30666e646bf5
Madhur Shrestha <madhurstha@gmail.com>
2025-06-06 13:22:22 +1000

    Changed to seeding database

--- backend/config/keys_prod.config.js
+++ backend/config/keys_prod.config.js
@@ -2,7 +2,7 @@ import dotenv from "dotenv";
 dotenv.config();
 
 export const keysProd = {
-  mongoURI: process.env.MONGO_URI,
+  mongoURI: process.env.MONGO_URI_SEED,
   secretOrKey: process.env.SECRET_OR_KEY,
 };
 
