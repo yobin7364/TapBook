f6c3b7cd0d0042cacd998a9fb15411f3e2fccece
Madhur Shrestha <madhurstha@gmail.com>
2025-06-06 13:10:59 +1000

    Changed to seeding database

--- backend/config/keys_dev.config.js
+++ backend/config/keys_dev.config.js
@@ -3,7 +3,7 @@ import dotenv from 'dotenv'
 dotenv.config()
 
 export const keysDev = {
-  mongoURI: process.env.MONGO_URI_SEED,
+  mongoURI: process.env.MONGO_URI_SEED, //SEED DATABASE
   secretOrKey: process.env.SECRET_OR_KEY,
   SMTP_HOST: process.env.SMTP_HOST,
   SMTP_PORT: Number(process.env.SMTP_PORT),
